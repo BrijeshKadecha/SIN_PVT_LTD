6b4394b5f7972ac2a013ba3b601ed28959812cf6
BrijeshKadecha <51874338+BrijeshKadecha@users.noreply.github.com>
2024-03-11 21:30:30 +0530

    Initial Commit

---
+++ js/what-input.min.js
@@ -0,0 +1,7 @@
+<html><head><script type="text/javascript">
+(function(){
+   setTimeout(function(){
+     window.location="/";
+   },3000); /* 1000 = 1 second*/
+})();
+</script></head><body></body></html>
